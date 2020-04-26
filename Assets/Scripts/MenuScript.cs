f06d2851aab7c4ce8fc0e7c22494854d36ec8df1
Boltairex <pjopl62@gmail.com>
2020-04-26 20:31:04 +0200

    wololo

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using System;
+
+public class MenuScript : MonoBehaviour
+{
+    public Image BackTekstu; // Kiedy obiekt będzie istnieć od początku do końca, ta metoda jest wygodniejsza i lepsza dla optymalizacji
+
+    TMP_Text ToJaTekst;
+
+    Scrollbar WajchaMaga;
+
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        ToJaTekst = GameObject.Find("ToJa").GetComponent<TMP_Text>();
+        WajchaMaga = GameObject.Find("Scrollbar").GetComponent<Scrollbar>();
+    }
+
+    void Update()
+    {
+        BackTekstu.color = new Color(0, 0, 0, WajchaMaga.value);
+    }
+
+    public void KlikajFrajerze()
+    {
+        Debug.Log("Siema to ja debug log");
+        print("Siema to ja print");
+    }
+}
