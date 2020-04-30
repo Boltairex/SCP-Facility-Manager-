5979a4ae0038946e8b74edb67103c76cb5a9e35a
matix044 <matix4676@gmail.com>
2020-04-30 23:04:25 +0200

    added simple gui

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+
+    public float speed;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = 10;
+
+        } else
+        {
+            speed = 3.5f;
+        }
+        transform.position += new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), Input.GetAxis("Mouse ScrollWheel")) * Time.deltaTime * speed;
+        transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Clamp(transform.position.z, -18.16f , -1.433f));
+
+    }
+}
---
+++ Assets/Scripts/LightController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightController : MonoBehaviour
+{
+
+    public bool iscontained;
+    Light light;
+    RaycastHit RayInfo;
+    // Start is called before the first frame update
+    void Start()
+    {
+        light = GameObject.Find("ContaimentLight").GetComponent<Light>();
+        light.intensity = 0.03f;
+        light.color = new Color(255, 255, 255, 255);
+
+    }
+
+    public void OnClick()
+    {
+        iscontained = !iscontained;
+
+        if (iscontained == false)
+        {
+            light.color = new Color(255, 255, 255, 255);
+
+        }
+        else
+        {
+            light.color = new Color(255, 69, 69, 255);
+        }
+    }
+
+
+}
